cc0eb95b2f2a577fe41a10ce981c5562ab5dfacb
3846masa <3846masahiro+git@gmail.com>
2018-10-03 23:39:21 +0900

    Add Layout and CameraPage

---
+++ src/components/camera/CameraPage/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import styles from './styles.css';
+import Layout from '~/components/common/Layout';
+
+const CameraPage = () => <Layout>CameraPage</Layout>;
+
+export default CameraPage;
