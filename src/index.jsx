9858a4f8a82fcf8050f6dea066ee1cd86005db93
3846masa <3846masahiro+git@gmail.com>
2018-10-03 17:40:51 +0900

    Add global.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import '~/development';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '~/global.css';
 import App from '~/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
