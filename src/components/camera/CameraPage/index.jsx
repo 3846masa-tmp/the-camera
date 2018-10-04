34a92bfaf92df8c1e4187a217b615769aea788dc
3846masa <3846masahiro+git@gmail.com>
2018-10-04 11:34:28 +0900

    Setup resolution

--- src/components/camera/CameraPage/index.jsx
+++ src/components/camera/CameraPage/index.jsx
@@ -6,29 +6,61 @@ import CameraView from '~/components/camera/CameraView';
 import CameraController from '~/components/camera/CameraController';
 import takePhotoFromStream from '~/helpers/takePhotoFromStream';
 import saveAs from '~/helpers/saveAs';
+import getConstraintsWithFacingMode from '~/helpers/getConstraintsWithFacingMode';
 
 /**
  * @typedef State
  * @property {MediaStream} stream
+ * @property {Record<string, *>} constraints
+ * @property {'user' | 'environment'} facingMode
  */
 
 class CameraPage extends React.Component {
   /** @type {State} */
   state = {
     stream: null,
+    constraints: {},
+    facingMode: null,
   };
 
   componentDidMount() {
-    this.updateStream();
+    this.initialize();
+  }
+
+  /** @param {State} prevState */
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.facingMode !== prevState.facingMode) {
+      this.updateStream();
+    }
+  }
+
+  async initialize() {
+    const constraints = {
+      user: await getConstraintsWithFacingMode('user'),
+      environment: await getConstraintsWithFacingMode('environment'),
+    };
+    const facingMode = constraints.environment ? 'environment' : 'user';
+    this.setState({ constraints, facingMode });
   }
 
   async updateStream() {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: { ideal: 'environment' },
-      },
-    });
-    this.setState({ stream });
+    this.closeStream();
+    const { constraints, facingMode } = this.state;
+    if (constraints[facingMode]) {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: constraints[facingMode].recommended,
+      });
+      this.setState({ stream });
+    }
+  }
+
+  closeStream() {
+    const { stream } = this.state;
+    if (stream) {
+      for (const track of stream.getTracks()) {
+        track.stop();
+      }
+    }
   }
 
   onClickShutter = async () => {
---
+++ src/helpers/getConstraintsWithFacingMode.js
@@ -0,0 +1,68 @@
+import createVideoElement from '~/helpers/createVideoElement';
+
+const ASPECT_RATIO = 16 / 9;
+
+const SIZE_CONSTRAINT = { min: 1, max: 7680, ideal: 7680 };
+
+/**
+ * @param {'user'  | 'environment'} facingMode
+ * @param {number} [aspectRatio]
+ */
+async function getConstraintsWithFacingMode(facingMode, aspectRatio = ASPECT_RATIO) {
+  /** @type {MediaStream} */
+  let stream;
+
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: { exact: facingMode },
+        width: SIZE_CONSTRAINT,
+        height: SIZE_CONSTRAINT,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+
+  // MediaTrack returns a part of all properties until playing
+  const videoElem = await createVideoElement(stream);
+  videoElem.remove();
+
+  const videoTrack = stream.getVideoTracks()[0];
+  const settings = videoTrack.getSettings();
+
+  const recommended = {
+    ...settings,
+    ...generateSize(settings, aspectRatio),
+    aspectRatio: { ideal: aspectRatio },
+  };
+
+  // Stop all tracks
+  for (const track of stream.getTracks()) {
+    track.stop();
+  }
+
+  return {
+    recommended,
+  };
+}
+
+function generateSize(settings, aspectRatio) {
+  const { width, height } = settings;
+  const longest = Math.max(width, height);
+  const narrowest = Math.min(width, height);
+
+  const maybeNarrow = longest / aspectRatio;
+  const maybeLong = narrowest * aspectRatio;
+
+  const ideal =
+    narrowest >= maybeNarrow ? { long: longest, narrow: maybeNarrow } : { long: maybeLong, narrow: narrowest };
+
+  return {
+    width: Math.round(width === longest ? ideal.long : ideal.narrow),
+    height: Math.round(height === longest ? ideal.long : ideal.narrow),
+  };
+}
+
+export default getConstraintsWithFacingMode;
