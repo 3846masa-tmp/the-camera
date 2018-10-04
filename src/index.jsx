d69a554ec542e5a0026ed27f21099de7b3696f76
3846masa <3846masahiro+git@gmail.com>
2018-10-05 07:30:45 +0900

    PWA

--- src/index.jsx
+++ src/index.jsx
@@ -6,3 +6,15 @@ import '~/global.css';
 import App from '~/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+if ('serviceWorker' in navigator) {
+  const swSrc = '/service-worker.js';
+  navigator.serviceWorker
+    .register(swSrc)
+    .then((registration) => {
+      console.log('ServiceWorker registration successful, scope is:', registration.scope);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
